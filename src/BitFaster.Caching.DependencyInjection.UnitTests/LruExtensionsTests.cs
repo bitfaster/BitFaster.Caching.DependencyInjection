ae646174c0abbfef6f4ccc1d9dc1e3f517b64d62
Alex Peck <alexpeck@microsoft.com>
2022-10-01 13:53:03 -0700

    no fail

--- src/BitFaster.Caching.DependencyInjection.UnitTests/LruExtensionsTests.cs
+++ src/BitFaster.Caching.DependencyInjection.UnitTests/LruExtensionsTests.cs
@@ -16,8 +16,6 @@ namespace BitFaster.Caching.DependencyInjection.UnitTests
 
             services.Count.Should().Be(1);
             services[0].IsSingleton<FastConcurrentLru<int, int>, ICache<int, int>>();
-
-            throw new Exception("failed");
         }
 
         [Fact]
